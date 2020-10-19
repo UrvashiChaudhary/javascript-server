3e9daf681d92f8282418fe6a28bb9f35bed1e216
UrvashiChaudhary <urvashi.chaudhary@successive.tech>
2020-10-19 15:40:32 -0800

    Docs: Prime.js updated

--- Programs10/Prime.js
+++ Programs10/Prime.js
@@ -1,8 +1,9 @@
 function primeNumber(num){
     let flag=true;
     if(num==0 || num==1){
-        console.log("neither prime nor composite");
+       console.log("neither prime nor composite");
     }
+    else{
     let lt=Math.ceil(num/2);
     
     for(let i=2;i<=lt;i++){
@@ -12,17 +13,18 @@ function primeNumber(num){
         }
         
     }
-    if(flag==true){
+    if(flag){
         console.log("Prime");
     }
     else{
         console.log("not prime");
     }
 }
+}
 
 
-//primeNumber(2);
-//primeNumber(3);
-primeNumber(4);
-//primeNumber(5);
-//primeNumber(6);
+primeNumber(1);
+primeNumber(3);
+primeNumber(0);
+primeNumber(5);
+primeNumber(6);
