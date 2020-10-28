45637440d411a568bce1a08d1992877f668de4ae
UrvashiChaudhary <urvashi.chaudhary@successive.tech>
2020-10-28 15:38:40 -0800

    Docs: Updated

--- dist/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-//import the server and config files
-const Server_1 = require("./Server");
-const configuration_1 = require("./config/configuration");
-console.log("config is ", configuration_1.default);
-const server = new Server_1.default({ PORT: 9000 });
-server.bootstrap().run();
-//# sourceMappingURL=index.js.map
--- dist/patterns/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.equilateralTriangle = exports.diamondPattern = void 0;
-const Diamond_1 = require("./Diamond");
-Object.defineProperty(exports, "diamondPattern", { enumerable: true, get: function () { return Diamond_1.diamondPattern; } });
-const Triangle_1 = require("./Triangle");
-Object.defineProperty(exports, "equilateralTriangle", { enumerable: true, get: function () { return Triangle_1.equilateralTriangle; } });
-//# sourceMappingURL=index.js.map
