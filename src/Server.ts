b2fb54550f4db8823b3f710bfe9e8795a2cd6cf9
UrvashiChaudhary <urvashi.chaudhary@successive.tech>
2020-11-03 15:30:01 -0800

    Express route-specific used in Server.ts

--- src/Server.ts
+++ src/Server.ts
@@ -22,7 +22,8 @@ app.use(errorHandler);
 return this;
 }
 initBodyParser() {
-    this.app.use(bodyParser.json({ type: 'application/*+json' }))
+    this.app.use(bodyParser.json())
+    this.app.use(bodyParser.urlencoded({ extended: false }))
 }
 run() {
 
