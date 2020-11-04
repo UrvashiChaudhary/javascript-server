04be48e795703d6aefdd205845c6ecc5140d5348
UrvashiChaudhary <urvashi.chaudhary@successive.tech>
2020-11-03 16:05:01 -0800

    Docs validationHandler.ts routes.ts, validation.ts added

--- src/controllers/trainee/routes.ts
+++ src/controllers/trainee/routes.ts
@@ -9,9 +9,9 @@ const traineeRouter = Router();
 
 traineeRouter.route('/')
     .get(validationHandler(validation.get),traineeController.get)
-    .post(traineeController.create)
-    .put(traineeController.update)
-    .delete(traineeController.update)
+    .post(validationHandler(validation.create),traineeController.create)
+    .put(validationHandler(validation.update),traineeController.update)
+    .delete(validationHandler(validation.delete),traineeController.update)
     //.get((req, res)=>{console.log("Inside get route"); res.send("Inside get route")})
     // .post((req, res)=>{console.log("Inside post route"); res.send("Inside post route")})
     // .put((req, res)=>{console.log("Inside put route"); res.send("Inside put route")})
