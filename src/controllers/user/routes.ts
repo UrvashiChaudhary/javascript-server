e212ef15f201798a0d1a745fd90b08221aba1cc1
Urvashi Chaudhary <urvashi.chaudhary@urvashic.successivetech.com>
2020-11-06 11:51:40 +0530

    validation.ts added

--- src/controllers/user/routes.ts
+++ src/controllers/user/routes.ts
@@ -3,15 +3,15 @@ import UserController from './Controller';
 
 import validationHandler from '../../libs/validationHandler';
 
-// import validation from './validation';
+ import validation from './validation';
 
 const UserRouter = Router();
 
 UserRouter.route('/')
-    .get(UserController.get)
-    .post(UserController.create)
-    .put(UserController.update)
-    .delete(UserController.update);
+    .get(validationHandler(validation.get),UserController.get)
+    .post(validationHandler(validation.create),UserController.create)
+    .put(validationHandler(validation.update),UserController.update)
+    .delete(validationHandler(validation.delete),UserController.update);
 // .get((req, res)=>{console.log("Inside get route"); res.send("Inside get route")})
 // .post((req, res)=>{console.log("Inside post route"); res.send("Inside post route")})
 // .put((req, res)=>{console.log("Inside put route"); res.send("Inside put route")})
