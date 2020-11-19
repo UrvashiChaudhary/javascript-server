6b7f1c66ce7c4205b3c9564c7f78e4d5aa4c82c7
Urvashi Chaudhary <urvashi.chaudhary@urvashic.successivetech.com>
2020-11-19 12:56:08 +0530

    latest pull from origin

--- src/controllers/user/Controller.ts
+++ src/controllers/user/Controller.ts
@@ -3,62 +3,63 @@ import { Request, Response, NextFunction } from 'express';
 import UserRepository from '../../repositories/user/UserRepository';
 import * as bcrypt from 'bcrypt';
 import config from '../../config/configuration';
-import {userModel} from '../../repositories/user/UserModel'
+import { userModel } from '../../repositories/user/UserModel';
 
 
 class UserController {
-static instance: UserController;
-static getInstance() {
-if (UserController.instance) {
-return UserController.instance;
-}
-UserController.instance = new UserController();
-return UserController.instance;
-}
-me(req: any, res: Response, next: NextFunction ) {
-const { user } = req;
-return res.status(200).send({ message: 'Me', status: 'ok', data: user});
-}
-login = (req: Request, res: Response, next: NextFunction) => {
+    static instance: UserController;
+    static getInstance() {
+        if (UserController.instance) {
+            return UserController.instance;
+        }
+        UserController.instance = new UserController();
+        return UserController.instance;
+    }
+    me(req: any, res: Response, next: NextFunction) {
+        const { user } = req;
+        return res.status(200).send({ message: 'Me', status: 'ok', data: user });
+    }
+    login = (req: Request, res: Response, next: NextFunction) => {
 
 
-try {
+        try {
 
 
-const { email, password } = req.body;
-console.log(email);
-userModel.findOne({ email: email }, (err, result) => {
-if (result) {
-if (password === result.password) {
+            const { email, password } = req.body;
+            console.log(email);
+            userModel.findOne({ email1: email }, (err, result) => {
+                if (result) {
+                    if (password === result.password) {
 
 
-result.password = bcrypt.hashSync(result.password, 10);
-const token = jwt.sign({ result }, 'xMi43lDEhAHie5lL5V6Sord0PJsim4UU');
-console.log(result);
-console.log(token);
-res.send({
-data: token,
-message: 'Login successfully',
-status: 200
-});
+                        result.password = bcrypt.hashSync(result.password, 10);
+                        const token = jwt.sign({ result }, 'xMi43lDEhAHie5lL5V6Sord0PJsim4UU');
+                        console.log(result);
+                        console.log(token);
+                        res.send({
+                            data: token,
+                            message: 'Login successfully',
+                            status: 200
+                        });
+                    }
+                    else {
+                        res.send({
+                            message: 'Password Doesnt Match',
+                            status: 400
+                        });
+                    }
+                }
+                else {
+                    res.send({
+                        message: 'Email is not Registered',
+                        status: 404
+                    });
+                }
+            });
+        }
+        catch (err) {
+            res.send(err);
+        }
+    }
 }
-else {
-res.send({
-message: 'Password Doesnt Match',
-status: 400
-});
-}
-}
-else {
-res.send({
-message: 'Email is not Registered',
-status: 404
-});
-}
-});
-}
-catch (err) {
-res.send(err);
-}
-}}
 export default UserController.getInstance();
